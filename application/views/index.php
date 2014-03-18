4ae277971315791c0ca37471bf90c978d1f589c7
Elton Oliveira <eltonknoxville@hotmail.com>
2014-03-18 16:01:43 -0400

    Correção CSS
    Correção CSS

--- application/views/index.php
+++ application/views/index.php
@@ -5,26 +5,23 @@
 
  		<title>:::AMBIENTAÇÃO - SENAC- AM:::</title>
  		<!--Bootstrap CSS -->
- 		<style type="text/css" src="<?=base_url()?>assets/bootstrap/css/bootstrap.css"></style>
- 		<style type="text/css" src="<?=base_url()?>assets/bootstrap/css/bootstrap.min.css"></style>
- 		<style type="text/css" src="<?=base_url()?>assets/bootstrap/css/bootstrap-theme.css"></style>
- 		<style type="text/css" src="<?=base_url()?>assets/bootstrap/css/bootstrap-theme.min.css"></style>
+ 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+ 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
  		<!--End Bootstrap CSS -->
  	</header>
  	<body>
- 		
+ 		<div class="container">
+ 			
+ 			<?=$contents;?>
 
- 		<?=$contents;?>
-
-
-
+		</div>
+	
  	</body>
  	<!--Jquery-->
  	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
  	<!--End Jquery--> 
  	<!--Script Boostrap -->
- 	<script type="text/javascript" src="<?=base_url(); ?>assets/bootstrap/js/bootstrap.js" ></script>
- 	<script type="text/javascript" src="<?=base_url(); ?>assets/bootstrap/js/bootstrap.min.js" ></script>
+ 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
  	<!--End Script Boostrap -->
  	<!--Custon -->
  	<script type="text/javascript" src="<?=base_url(); ?>assets/js/script.js"></script>
