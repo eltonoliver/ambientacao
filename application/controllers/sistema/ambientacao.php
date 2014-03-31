d17fef5dc854f397826d1d59e566cb61bac89de4
Elton Oliveira <eltonknoxville@hotmail.com>
2014-03-31 10:12:10 -0400

    Mudança do nome das pastas
    Mudança do nome das pastas

--- application/controllers/sistema/ambientacao.php
+++ application/controllers/sistema/ambientacao.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class System extends CI_Controller {
+class Ambientacao extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
