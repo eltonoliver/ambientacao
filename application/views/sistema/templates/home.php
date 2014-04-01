e5a40005dd698f76b4b87d6bb78c58dc32c6cfb2
Elton <eltonknoxville@hotmail.com>
2014-04-01 08:21:31 -0400

    Altercçao casa
    Altercçao casa

--- application/views/sistema/templates/home.php
+++ application/views/sistema/templates/home.php
@@ -1,3 +1,3 @@
   <!--HOME - SISTEMA -->	
-  <div class="col-xs-12 col-md-8" id="ambiente" style="overflow:auto;height:650px;border:red 1px solid;"><!-- AJAX --> </div>
-<div class="col-md-12 col-md-offset 13" style="border:red 1px solid;" > <!--CONTROLE --><center>CONTROLLE </center></div><!--CONTROLE -->
+  <div class="col-xs-12 col-md-8" id="ambiente" style="overflow:auto;height:650px;"><!-- AJAX --> </div>
+<div class="col-md-12 col-md-offset 13"  > <!--CONTROLE --><center>CONTROLLE </center></div><!--CONTROLE -->
