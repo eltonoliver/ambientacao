13bba50d8413e2c34957a9fa2db26fa2097e2f50
Elton <eltonknoxville@hotmail.com>
2014-04-01 00:53:31 -0400

    Alteração na visualização do conteúdo
    Alteração na visualização do conteúdo

--- application/controllers/sistema/ambientacao.php
+++ application/controllers/sistema/ambientacao.php
@@ -61,7 +61,15 @@ class Ambientacao extends CI_Controller {
 										echo $questao;
 									}
 
-
+								 echo '
+								 
+								 		<br><br><br>
+								 		<input type="hidden" name="idquestao" value="'.$value->id.'" />
+								 		<input type="hidden" name="idcategoria" value="'.$value->tbl_categoria_id.'" />
+								 		<input type="hidden" name="idfuncionario" value="100" />
+      									<button type="submit" class="btn btn-default">Enviar</button>
+    								
+								 ';	
 							echo form_close();		
 								/*********************QUESTOES*********************/	
 
@@ -74,27 +82,10 @@ class Ambientacao extends CI_Controller {
 
 					echo '
 
-						<div style="font-size:25px;font-weight: bold;color:white;" class="bg-primary"><center> Título </center></div>
+						<div style="font-size:25px;font-weight: bold;color:white;" class="bg-primary"><center> '.$value->titulo.' </center></div>
 						<div class="bg-warning" style="text-align:justify;padding:10px 10px 10px 10px ;height:600px;">
-								Conteúdo <h1>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h1>
 								
-								<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-								
-								<h2>Header Level 2</h2>
-								
-								<ol>
-									<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-									<li>Aliquam tincidunt mauris eu risus.</li>
-								</ol>
-								
-								<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-								
-								<h3>Header Level 3</h3>
-								
-								<ul>
-									<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-									<li>Aliquam tincidunt mauris eu risus.</li>
-								</ul>
+								'.$value->conteudo.'
 						
 						
 						</div>
